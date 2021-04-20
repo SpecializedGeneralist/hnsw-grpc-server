c00ea342a2d19b09b755a5470c2661446fb8db90
marco-nicola <marconicola@disroot.org>
2021-04-20 19:46:30 +0200

    Add and improve hnswgo tests

--- pkg/internal/hnswgo/hnsw_test.go
+++ pkg/internal/hnswgo/hnsw_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/SpecializedGeneralist/hnsw-grpc-server/pkg/internal/hnswgo"
 	"github.com/stretchr/testify/assert"
 	"os"
+	"path"
 	"testing"
 )
 
@@ -27,37 +28,40 @@ var sampleVectors = [][]float32{
 }
 
 func TestSpaceTypeFromString(t *testing.T) {
+	t.Parallel()
+
 	t.Run("IPSpace", func(t *testing.T) {
+		t.Parallel()
 		val, err := hnswgo.SpaceTypeFromString("ip")
 		assert.NoError(t, err)
 		assert.Equal(t, hnswgo.IPSpace, val)
 	})
+
 	t.Run("CosineSpace", func(t *testing.T) {
+		t.Parallel()
 		val, err := hnswgo.SpaceTypeFromString("cosine")
 		assert.NoError(t, err)
 		assert.Equal(t, hnswgo.CosineSpace, val)
 	})
+
 	t.Run("L2Space", func(t *testing.T) {
+		t.Parallel()
 		val, err := hnswgo.SpaceTypeFromString("l2")
 		assert.NoError(t, err)
 		assert.Equal(t, hnswgo.L2Space, val)
 	})
+
 	t.Run("invalid", func(t *testing.T) {
+		t.Parallel()
 		_, err := hnswgo.SpaceTypeFromString("foo")
 		assert.Error(t, err)
 	})
 }
 
 func TestHNSW_IPSpace(t *testing.T) {
-	hnsw := hnswgo.New(hnswgo.Config{
-		SpaceType:      hnswgo.IPSpace,
-		Dim:            5,
-		MaxElements:    10,
-		M:              10,
-		EfConstruction: 200,
-		RandSeed:       100,
-		AutoIDEnabled:  false,
-	})
+	t.Parallel()
+
+	hnsw := hnswgo.New(makeConfig(hnswgo.IPSpace, false))
 
 	for i, vector := range sampleVectors {
 		err := hnsw.AddPoint(vector, uint32(i))
@@ -69,15 +73,9 @@ func TestHNSW_IPSpace(t *testing.T) {
 }
 
 func TestHNSW_L2Space(t *testing.T) {
-	hnsw := hnswgo.New(hnswgo.Config{
-		SpaceType:      hnswgo.L2Space,
-		Dim:            5,
-		MaxElements:    10,
-		M:              10,
-		EfConstruction: 200,
-		RandSeed:       100,
-		AutoIDEnabled:  false,
-	})
+	t.Parallel()
+
+	hnsw := hnswgo.New(makeConfig(hnswgo.L2Space, false))
 
 	for i, vector := range sampleVectors {
 		err := hnsw.AddPoint(vector, uint32(i))
@@ -89,15 +87,9 @@ func TestHNSW_L2Space(t *testing.T) {
 }
 
 func TestHNSW_CosineSpace(t *testing.T) {
-	hnsw := hnswgo.New(hnswgo.Config{
-		SpaceType:      hnswgo.CosineSpace,
-		Dim:            5,
-		MaxElements:    10,
-		M:              10,
-		EfConstruction: 200,
-		RandSeed:       100,
-		AutoIDEnabled:  false,
-	})
+	t.Parallel()
+
+	hnsw := hnswgo.New(makeConfig(hnswgo.CosineSpace, false))
 
 	for i, vector := range sampleVectors {
 		err := hnsw.AddPoint(vector, uint32(i))
@@ -120,15 +112,9 @@ func TestHNSW_CosineSpace(t *testing.T) {
 }
 
 func TestHNSW_AutoIDDisabled(t *testing.T) {
-	hnsw := hnswgo.New(hnswgo.Config{
-		SpaceType:      hnswgo.CosineSpace,
-		Dim:            5,
-		MaxElements:    10,
-		M:              10,
-		EfConstruction: 200,
-		RandSeed:       100,
-		AutoIDEnabled:  false,
-	})
+	t.Parallel()
+
+	hnsw := hnswgo.New(makeConfig(hnswgo.CosineSpace, false))
 
 	err := hnsw.AddPoint(sampleVectors[0], uint32(42))
 	assert.NoError(t, err)
@@ -143,15 +129,9 @@ func TestHNSW_AutoIDDisabled(t *testing.T) {
 }
 
 func TestHNSW_AutoIDEnabled(t *testing.T) {
-	hnsw := hnswgo.New(hnswgo.Config{
-		SpaceType:      hnswgo.CosineSpace,
-		Dim:            5,
-		MaxElements:    10,
-		M:              10,
-		EfConstruction: 200,
-		RandSeed:       100,
-		AutoIDEnabled:  true,
-	})
+	t.Parallel()
+
+	hnsw := hnswgo.New(makeConfig(hnswgo.CosineSpace, true))
 
 	for _, vector := range sampleVectors {
 		err := hnsw.AddPointAutoID(vector)
@@ -167,15 +147,9 @@ func TestHNSW_AutoIDEnabled(t *testing.T) {
 }
 
 func TestHNSW_MarkDelete(t *testing.T) {
-	hnsw := hnswgo.New(hnswgo.Config{
-		SpaceType:      hnswgo.CosineSpace,
-		Dim:            5,
-		MaxElements:    10,
-		M:              10,
-		EfConstruction: 200,
-		RandSeed:       100,
-		AutoIDEnabled:  false,
-	})
+	t.Parallel()
+
+	hnsw := hnswgo.New(makeConfig(hnswgo.CosineSpace, false))
 
 	for i, vector := range sampleVectors {
 		err := hnsw.AddPoint(vector, uint32(i))
@@ -192,41 +166,150 @@ func TestHNSW_MarkDelete(t *testing.T) {
 }
 
 func TestHNSW_SaveAndLoad(t *testing.T) {
-	tmpPath, err := os.MkdirTemp("", "hnsw_test")
-	assert.NoError(t, err)
-	defer func() {
-		err = os.RemoveAll(tmpPath)
-		assert.NoError(t, err)
-	}()
+	t.Parallel()
+
+	dir := createTempDir(t)
+	defer deleteDir(t, dir)
 
 	var originalResults []hnswgo.KNNResult
 
 	{
-		hnsw := hnswgo.New(hnswgo.Config{
-			SpaceType:      hnswgo.CosineSpace,
-			Dim:            5,
-			MaxElements:    10,
-			M:              10,
-			EfConstruction: 200,
-			RandSeed:       100,
-			AutoIDEnabled:  false,
-		})
+		hnsw := hnswgo.New(makeConfig(hnswgo.CosineSpace, false))
 
 		for i, vector := range sampleVectors {
-			err = hnsw.AddPoint(vector, uint32(i))
+			err := hnsw.AddPoint(vector, uint32(i))
 			assert.NoError(t, err)
 		}
 
 		originalResults = hnsw.SearchKNN(sampleVectors[0], 2)
 		assert.Len(t, originalResults, 2)
 
-		err = hnsw.Save(tmpPath)
+		err := hnsw.Save(dir)
 		assert.NoError(t, err)
 	}
 
-	hnsw, err := hnswgo.Load(tmpPath)
+	hnsw, err := hnswgo.Load(dir)
 	assert.NoError(t, err)
 
 	newResults := hnsw.SearchKNN(sampleVectors[0], 2)
 	assert.Equal(t, originalResults, newResults)
+}
+
+func TestHNSW_LoadingErrors(t *testing.T) {
+	t.Parallel()
+
+	t.Run("state.tmp exists", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		createAndSaveSampleIndex(t, dir)
+		err := os.Rename(path.Join(dir, "state"), path.Join(dir, "state.tmp"))
+		assert.NoError(t, err)
+
+		hnsw, err := hnswgo.Load(dir)
+		assert.Nil(t, hnsw)
+		assert.Error(t, err)
+	})
+
+	t.Run("state does not exist", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		createAndSaveSampleIndex(t, dir)
+		err := os.Remove(path.Join(dir, "state"))
+		assert.NoError(t, err)
+
+		hnsw, err := hnswgo.Load(dir)
+		assert.Nil(t, hnsw)
+		assert.Error(t, err)
+	})
+
+	t.Run("index.tmp exists", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		createAndSaveSampleIndex(t, dir)
+		err := os.Rename(path.Join(dir, "index"), path.Join(dir, "index.tmp"))
+		assert.NoError(t, err)
+
+		hnsw, err := hnswgo.Load(dir)
+		assert.Nil(t, hnsw)
+		assert.Error(t, err)
+	})
+
+	t.Run("index does not exist", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		createAndSaveSampleIndex(t, dir)
+		err := os.Remove(path.Join(dir, "index"))
+		assert.NoError(t, err)
+
+		hnsw, err := hnswgo.Load(dir)
+		assert.Nil(t, hnsw)
+		assert.Error(t, err)
+	})
+
+	t.Run("the path does not exist", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		hnsw, err := hnswgo.Load(path.Join(dir, "foo"))
+		assert.Nil(t, hnsw)
+		assert.Error(t, err)
+	})
+}
+
+func TestHNSW_SavingErrors(t *testing.T) {
+	t.Parallel()
+
+	t.Run("the path does not exist", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		hnsw := hnswgo.New(makeConfig(hnswgo.CosineSpace, true))
+		err := hnsw.Save(path.Join(dir, "foo", "bar"))
+		assert.Error(t, err)
+	})
+}
+func createAndSaveSampleIndex(t *testing.T, dir string) {
+	hnsw := hnswgo.New(makeConfig(hnswgo.CosineSpace, true))
+	for _, vector := range sampleVectors {
+		err := hnsw.AddPointAutoID(vector)
+		assert.NoError(t, err)
+	}
+	err := hnsw.Save(dir)
+	assert.NoError(t, err)
+
+	assert.FileExists(t, path.Join(dir, "state"))
+	assert.FileExists(t, path.Join(dir, "index"))
+}
+
+func makeConfig(spaceType hnswgo.SpaceType, autoIDEnabled bool) hnswgo.Config {
+	return hnswgo.Config{
+		SpaceType:      spaceType,
+		Dim:            5,
+		MaxElements:    10,
+		M:              10,
+		EfConstruction: 200,
+		RandSeed:       100,
+		AutoIDEnabled:  autoIDEnabled,
+	}
+}
+
+func createTempDir(t *testing.T) string {
+	dir, err := os.MkdirTemp("", "hnsw_test")
+	assert.NoError(t, err)
+	return dir
+}
+
+func deleteDir(t *testing.T, dir string) {
+	err := os.RemoveAll(dir)
+	assert.NoError(t, err)
 }
