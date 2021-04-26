32dc9eed2127fa9181846833cd9f8286810ee117
Marco Nicola <marconicola@disroot.org>
2021-04-26 19:39:00 +0200

    Create indexmanager package

---
+++ pkg/indexmanager/indexmanager.go
@@ -0,0 +1,173 @@
+// Copyright 2021 SpecializedGeneralist
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package indexmanager
+
+import (
+	"fmt"
+	"github.com/SpecializedGeneralist/hnsw-grpc-server/pkg/hnswgo"
+	"github.com/SpecializedGeneralist/hnsw-grpc-server/pkg/osutils"
+	"github.com/rs/zerolog"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+	"sync"
+)
+
+var indexNameRegexp = regexp.MustCompile(`^[a-zA-Z0-9_-]+$`)
+
+// IndexManager allows easy handling of multiple HNSW indices.
+type IndexManager struct {
+	path    string
+	logger  zerolog.Logger
+	indices map[string]*hnswgo.HNSW
+	rwMx    sync.RWMutex
+}
+
+// New creates a new IndexManager.
+func New(path string, logger zerolog.Logger) *IndexManager {
+	return &IndexManager{
+		path:    path,
+		logger:  logger,
+		indices: make(map[string]*hnswgo.HNSW),
+		rwMx:    sync.RWMutex{},
+	}
+}
+
+// LoadIndices load all HNSW indices stored in the configured path.
+func (im *IndexManager) LoadIndices() error {
+	im.rwMx.Lock()
+	defer im.rwMx.Unlock()
+
+	im.logger.Info().Msgf("loading all indices from dir %#v...", im.path)
+	files, err := os.ReadDir(im.path)
+	if err != nil {
+		return fmt.Errorf("error reading content of indices dir %#v: %w", im.path, err)
+	}
+
+	for _, file := range files {
+		name := file.Name()
+
+		// Ignore files and hidden dirs
+		if !file.IsDir() || strings.HasPrefix(name, ".") {
+			continue
+		}
+
+		im.logger.Info().Msgf("loading index %#v...", name)
+		err = im.loadIndex(name)
+		if err != nil {
+			return err
+		}
+	}
+
+	im.logger.Info().Msg("all indices successfully loaded")
+	return nil
+}
+
+// GetIndex returns the in-memory HNSW index (if it exists) and reports
+// whether it is found.
+func (im *IndexManager) GetIndex(name string) (*hnswgo.HNSW, bool) {
+	im.rwMx.RLock()
+	defer im.rwMx.RUnlock()
+
+	index, found := im.indices[name]
+	return index, found
+}
+
+// CreateIndex creates a new in-memory index with the given name.
+// If the name is not acceptable or an index with the same name already
+// exists, an error is returned.
+func (im *IndexManager) CreateIndex(name string, config hnswgo.Config) (*hnswgo.HNSW, error) {
+	im.rwMx.Lock()
+	defer im.rwMx.Unlock()
+
+	if !isValidIndexName(name) {
+		return nil, fmt.Errorf("invalid index name")
+	}
+	if _, ok := im.indices[name]; ok {
+		return nil, fmt.Errorf("index with name %#v already exists", name)
+	}
+
+	index := hnswgo.New(config)
+	im.indices[name] = index
+	return index, nil
+}
+
+// PersistIndex saves the current index to disk.
+func (im *IndexManager) PersistIndex(name string) error {
+	im.rwMx.RLock()
+	defer im.rwMx.RUnlock()
+
+	index, indexExists := im.indices[name]
+	if !indexExists {
+		return fmt.Errorf("index does not exist")
+	}
+	err := index.Save(path.Join(im.path, name))
+	if err != nil {
+		return fmt.Errorf("error persisting index %#v: %w", name, err)
+	}
+	return nil
+}
+
+// DeleteIndex remove an index from the in-memory storage and, if the index was
+// persisted, also from disk.
+func (im *IndexManager) DeleteIndex(name string) error {
+	im.rwMx.Lock()
+	defer im.rwMx.Unlock()
+
+	if _, ok := im.indices[name]; !ok {
+		return fmt.Errorf("index does not exist")
+	}
+
+	filename := path.Join(im.path, name)
+	dirExists, err := osutils.DirExists(filename)
+	if err != nil {
+		return err
+	}
+	if dirExists {
+		err = os.RemoveAll(filename)
+		if err != nil {
+			return fmt.Errorf("error removing index dir %#v: %w", filename, err)
+		}
+	}
+
+	delete(im.indices, name)
+	return nil
+}
+
+// Size returns the amount of currently loaded indices.
+func (im *IndexManager) Size() int {
+	im.rwMx.RLock()
+	defer im.rwMx.RUnlock()
+
+	return len(im.indices)
+}
+
+func (im *IndexManager) loadIndex(name string) error {
+	if _, ok := im.indices[name]; ok {
+		return fmt.Errorf("index %#v was already loaded", name)
+	}
+
+	h, err := hnswgo.Load(path.Join(im.path, name))
+	if err != nil {
+		return fmt.Errorf("error loading index %#v: %w", name, err)
+	}
+	im.indices[name] = h
+	return nil
+}
+
+func isValidIndexName(name string) bool {
+	return len(name) <= 255 && indexNameRegexp.MatchString(name)
+}
---
+++ pkg/indexmanager/indexmanager_test.go
@@ -0,0 +1,363 @@
+// Copyright 2021 SpecializedGeneralist
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package indexmanager_test
+
+import (
+	"github.com/SpecializedGeneralist/hnsw-grpc-server/pkg/hnswgo"
+	"github.com/SpecializedGeneralist/hnsw-grpc-server/pkg/indexmanager"
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"path"
+	"testing"
+)
+
+var sampleConfig = hnswgo.Config{
+	SpaceType:      hnswgo.CosineSpace,
+	Dim:            5,
+	MaxElements:    10,
+	M:              10,
+	EfConstruction: 200,
+	RandSeed:       100,
+	AutoIDEnabled:  true,
+}
+
+func TestIndexManager_LoadIndices(t *testing.T) {
+	t.Parallel()
+
+	t.Run("missing directory", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		im := indexmanager.New(path.Join(dir, "foo"), zerolog.Nop())
+		err := im.LoadIndices()
+		assert.Error(t, err)
+	})
+
+	t.Run("empty directory", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		im := indexmanager.New(dir, zerolog.Nop())
+		err := im.LoadIndices()
+		assert.NoError(t, err)
+		assert.Equal(t, 0, im.Size())
+	})
+
+	t.Run("it ignores files and hidden directories", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		file, err := os.Create(path.Join(dir, "file"))
+		assert.NoError(t, err)
+		err = file.Close()
+		assert.NoError(t, err)
+
+		err = os.Mkdir(path.Join(dir, ".hidden-dir"), 0777)
+		assert.NoError(t, err)
+
+		im := indexmanager.New(dir, zerolog.Nop())
+		err = im.LoadIndices()
+		assert.NoError(t, err)
+		assert.Equal(t, 0, im.Size())
+	})
+
+	t.Run("indices with errors", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		// An empty directory is surely not a valid index
+		err := os.Mkdir(path.Join(dir, "not-an-index"), 0777)
+		assert.NoError(t, err)
+
+		im := indexmanager.New(dir, zerolog.Nop())
+		err = im.LoadIndices()
+		assert.Error(t, err)
+	})
+
+	t.Run("existing indices", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		createAndSaveSampleIndex(t, path.Join(dir, "foo"))
+		createAndSaveSampleIndex(t, path.Join(dir, "bar"))
+
+		im := indexmanager.New(dir, zerolog.Nop())
+		err := im.LoadIndices()
+		assert.NoError(t, err)
+		assert.Equal(t, 2, im.Size())
+
+		index, found := im.GetIndex("foo")
+		assert.NotNil(t, index)
+		assert.True(t, found)
+
+		index, found = im.GetIndex("bar")
+		assert.NotNil(t, index)
+		assert.True(t, found)
+
+		index, found = im.GetIndex("baz")
+		assert.Nil(t, index)
+		assert.False(t, found)
+	})
+}
+
+func TestIndexManager_CreateIndex(t *testing.T) {
+	t.Parallel()
+
+	t.Run("creating valid index", func(t *testing.T) {
+		t.Parallel()
+		im := indexmanager.New(os.TempDir(), zerolog.Nop())
+
+		index, err := im.CreateIndex("foo", sampleConfig)
+		assert.NoError(t, err)
+		assert.NotNil(t, index)
+
+		index, err = im.CreateIndex("bar", sampleConfig)
+		assert.NoError(t, err)
+		assert.NotNil(t, index)
+
+		index, found := im.GetIndex("foo")
+		assert.NotNil(t, index)
+		assert.True(t, found)
+
+		index, found = im.GetIndex("bar")
+		assert.NotNil(t, index)
+		assert.True(t, found)
+
+		index, found = im.GetIndex("baz")
+		assert.Nil(t, index)
+		assert.False(t, found)
+	})
+
+	t.Run("invalid name", func(t *testing.T) {
+		t.Parallel()
+		im := indexmanager.New(os.TempDir(), zerolog.Nop())
+		index, err := im.CreateIndex("foo!?", sampleConfig)
+		assert.Error(t, err)
+		assert.Nil(t, index)
+	})
+
+	t.Run("index already exists", func(t *testing.T) {
+		t.Parallel()
+		im := indexmanager.New(os.TempDir(), zerolog.Nop())
+
+		index, err := im.CreateIndex("foo", sampleConfig)
+		assert.NoError(t, err)
+		assert.NotNil(t, index)
+
+		index, err = im.CreateIndex("foo", sampleConfig)
+		assert.Error(t, err)
+		assert.Nil(t, index)
+	})
+}
+
+func TestIndexManager_PersistIndex(t *testing.T) {
+	t.Parallel()
+
+	t.Run("successful persistence", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+
+		{
+			im := indexmanager.New(dir, zerolog.Nop())
+
+			foo, err := im.CreateIndex("foo", sampleConfig)
+			assert.NoError(t, err)
+			err = foo.AddPointAutoID(sampleVectors[0])
+			assert.NoError(t, err)
+
+			bar, err := im.CreateIndex("bar", sampleConfig)
+			assert.NoError(t, err)
+			err = bar.AddPointAutoID(sampleVectors[1])
+			assert.NoError(t, err)
+
+			err = im.PersistIndex("foo")
+			assert.NoError(t, err)
+
+			err = im.PersistIndex("bar")
+			assert.NoError(t, err)
+		}
+		{
+			im := indexmanager.New(dir, zerolog.Nop())
+			err := im.LoadIndices()
+			assert.NoError(t, err)
+			assert.Equal(t, 2, im.Size())
+
+			index, found := im.GetIndex("foo")
+			assert.NotNil(t, index)
+			assert.True(t, found)
+
+			index, found = im.GetIndex("bar")
+			assert.NotNil(t, index)
+			assert.True(t, found)
+		}
+	})
+
+	t.Run("index does not exist", func(t *testing.T) {
+		t.Parallel()
+		im := indexmanager.New(os.TempDir(), zerolog.Nop())
+
+		err := im.PersistIndex("foo")
+		assert.Error(t, err)
+	})
+
+	t.Run("saving error", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+		im := indexmanager.New(path.Join(dir, "foo"), zerolog.Nop())
+
+		_, err := im.CreateIndex("bar", sampleConfig)
+		assert.NoError(t, err)
+
+		err = im.PersistIndex("bar")
+		assert.Error(t, err)
+	})
+}
+
+func TestIndexManager_DeleteIndex(t *testing.T) {
+	t.Parallel()
+
+	t.Run("deleting non persisted index", func(t *testing.T) {
+		t.Parallel()
+		im := indexmanager.New(os.TempDir(), zerolog.Nop())
+
+		_, err := im.CreateIndex("bar", sampleConfig)
+		assert.NoError(t, err)
+
+		index, found := im.GetIndex("bar")
+		assert.NotNil(t, index)
+		assert.True(t, found)
+
+		err = im.DeleteIndex("bar")
+		assert.NoError(t, err)
+
+		index, found = im.GetIndex("bar")
+		assert.Nil(t, index)
+		assert.False(t, found)
+	})
+
+	t.Run("deleting persisted index", func(t *testing.T) {
+		t.Parallel()
+		dir := createTempDir(t)
+		defer deleteDir(t, dir)
+		im := indexmanager.New(dir, zerolog.Nop())
+
+		_, err := im.CreateIndex("foo", sampleConfig)
+		assert.NoError(t, err)
+		err = im.PersistIndex("foo")
+		assert.NoError(t, err)
+
+		_, err = im.CreateIndex("bar", sampleConfig)
+		assert.NoError(t, err)
+		err = im.PersistIndex("bar")
+		assert.NoError(t, err)
+
+		index, found := im.GetIndex("foo")
+		assert.NotNil(t, index)
+		assert.True(t, found)
+
+		index, found = im.GetIndex("bar")
+		assert.NotNil(t, index)
+		assert.True(t, found)
+
+		assert.DirExists(t, path.Join(dir, "foo"))
+		assert.DirExists(t, path.Join(dir, "bar"))
+
+		err = im.DeleteIndex("foo")
+		assert.NoError(t, err)
+
+		index, found = im.GetIndex("foo")
+		assert.Nil(t, index)
+		assert.False(t, found)
+
+		index, found = im.GetIndex("bar")
+		assert.NotNil(t, index)
+		assert.True(t, found)
+
+		assert.NoDirExists(t, path.Join(dir, "foo"))
+		assert.DirExists(t, path.Join(dir, "bar"))
+	})
+
+	t.Run("error checking if index dir exists", func(t *testing.T) {
+		t.Parallel()
+		filename := createTempFile(t)
+		defer deleteFile(t, filename)
+		im := indexmanager.New(filename, zerolog.Nop())
+
+		_, err := im.CreateIndex("bar", sampleConfig)
+		assert.NoError(t, err)
+
+		err = im.DeleteIndex("bar")
+		assert.Error(t, err)
+	})
+
+	t.Run("index does not exist", func(t *testing.T) {
+		t.Parallel()
+		im := indexmanager.New(os.TempDir(), zerolog.Nop())
+
+		err := im.DeleteIndex("foo")
+		assert.Error(t, err)
+	})
+}
+
+var sampleVectors = [][]float32{
+	{0.1, 0.2, 0.3, 0.4, 0.5},
+	{0.9, 0.8, 0.7, 0.6, 0.5},
+}
+
+func createAndSaveSampleIndex(t *testing.T, dir string) {
+	hnsw := hnswgo.New(sampleConfig)
+	for _, vector := range sampleVectors {
+		err := hnsw.AddPointAutoID(vector)
+		assert.NoError(t, err)
+	}
+	err := hnsw.Save(dir)
+	assert.NoError(t, err)
+
+	assert.FileExists(t, path.Join(dir, "state"))
+	assert.FileExists(t, path.Join(dir, "index"))
+}
+
+func createTempDir(t *testing.T) string {
+	dir, err := os.MkdirTemp("", "indexmanager_test")
+	assert.NoError(t, err)
+	return dir
+}
+
+func deleteDir(t *testing.T, dir string) {
+	err := os.RemoveAll(dir)
+	assert.NoError(t, err)
+}
+
+func createTempFile(t *testing.T) string {
+	file, err := os.CreateTemp("", "indexmanager_test")
+	assert.NoError(t, err)
+	filename := file.Name()
+	err = file.Close()
+	assert.NoError(t, err)
+	return filename
+}
+
+func deleteFile(t *testing.T, name string) {
+	err := os.Remove(name)
+	assert.NoError(t, err)
+}
